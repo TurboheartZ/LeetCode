91f64914ecd04061e5c3868383aa07fd97307ccf
TurboheartZ <zhangxinyi90@gmail.com>
2016-04-13 16:33:16 -0700

    Revert "3rd Summit"
    This reverts commit b3482b2b79714df626d8ef4b8a6bbb46c1dfbeb7.

--- Intersection Lines/src/Point.java
+++
@@ -1,9 +0,0 @@
-
-public class Point {
-	public double x;
-	public double y;
-	public Point(double xValue, double yValue){
-		this.x = xValue;
-		this.y = yValue;
-	}
-}
--- Living People/src/Span.java
+++
@@ -1,9 +0,0 @@
-
-public class Span {
-	int birth = -1;
-	int dead = -1;
-	public Span(int x,int y){
-		this.birth = x;
-		this.dead = y;
-	}
-}
